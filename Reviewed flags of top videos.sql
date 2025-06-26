adcaa5b85f086fc5aa7cd937f0f0dec756c7b6e5
megssingh27 <meghana.singh275@gmail.com>
2025-06-26 23:49:28 +0530

    Create Reviewed flags of top videos.sql

---
+++ Reviewed flags of top videos.sql
@@ -0,0 +1,12 @@
+/*
+Hard
+
+Reviewed flags of top videos
+
+For the video (or videos) that received the most user flags, how many of these flags were reviewed by YouTube? Output the video ID and the corresponding number of reviewed flags.
+
+Tables
+user_flags
+flag_review
+*/
+
