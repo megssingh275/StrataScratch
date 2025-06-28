603085a8851ff9d91442c524ff1c442e3891986d
megssingh27 <meghana.singh275@gmail.com>
2025-06-28 20:38:27 +0530

    Update Reviewed flags of top videos.sql

--- Reviewed flags of top videos.sql
+++ Reviewed flags of top videos.sql
@@ -10,3 +10,16 @@ user_flags
 flag_review
 */
 
+
+with t as (
+    select 
+        t1.video_id,
+        rank() over(order by count(t1.flag_id) desc) as flags_cnt_rank, 
+        sum(coalesce(reviewed_by_yt, 0)) yt_cnt
+    from user_flags t1
+    join flag_review t2
+        on t1.flag_id = t2.flag_id
+    group by t1.video_id
+)
+
+select video_id, yt_cnt from t where flags_cnt_rank = 1
